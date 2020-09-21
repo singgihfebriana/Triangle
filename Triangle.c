bc2b943eb1464fc568f175f246758bc6f6fbc33c
Singgih Febriana <64336941+singgihfebriana@users.noreply.github.com>
2020-09-21 21:12:10 +0700

    Add files via upload

---
+++ Triangle.c
@@ -0,0 +1,39 @@
+//*Name    : Singgih Febriana
+//*Program : Triangle
+//*Date    : September 21,2020
+
+#include<stdio.h>
+#include<windows.h>
+
+main(){
+	int i,j,k,l,m,n,o,p,q;
+	for (l=0;l<=4;l++){
+		printf(" ");
+	}
+	printf("*\n");
+	for (k=0;k<=3;k++){
+		printf(" ");
+	}
+	for (i=0;i<=2;i++){
+		printf("*");
+	}
+	printf("\n");
+	for (m=0;m<=2;m++){
+		printf(" ");
+	}
+	for (j=0;j<=4;j++){
+		printf("*");
+	}
+	printf("\n");
+	for(p=0;p<=1;p++){
+		printf(" ");
+	}
+	for (n=0;n<=6;n++){
+		printf("*");
+	}
+	printf("\n");
+	printf(" ");
+	for (q=0;q<=8;q++){
+		printf("*");
+	}
+}
